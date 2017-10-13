835f55c1ee7a86ca319cc3a31080d12003fc1a2c
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-10-13 23:13:14 +0200

    Simplify metrics to svg

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const badge = require('gh-badges');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 const color_ranges = {
   95: 'brightgreen',
@@ -25,30 +27,24 @@ const get_color_for_percentage = async (percentage) => {
   return color;
 };
 
-const metrics_to_svg = (lighthouse_metrics) => {
-  Object.keys(lighthouse_metrics).forEach(description => {
-    const generated_svg = get_color_for_percentage(lighthouse_metrics[description]).then(color => {
-      return new Promise((res, err) => {
-        badge({
-          text: [description, Math.round(lighthouse_metrics[description]) + '%'],
-          colorscheme: color,
-          template: "flat"
-        }, (svg, err) => {
-          return res(svg);
-        });
-      });
-    });
+async function metrics_to_svg(lighthouse_metrics) {
+  for (let description of Object.keys(lighthouse_metrics)) {
+    const badge_color = await get_color_for_percentage(lighthouse_metrics[description]);
 
-    generated_svg.then((svg) => {
+    badge({
+      text: [description, Math.round(lighthouse_metrics[description]) + '%'],
+      colorscheme: badge_color,
+      template: "flat"
+    }, (svg, err) => {
       fs.writeFile(path.join(__dirname, 'assets', 'badges', description + '.svg'), svg, (err) => {
       })
     });
-  });
-};
+  }
+}
 
 badge.loadFont(path.join(__dirname, 'assets', 'fonts', 'Verdana.ttf'), (err) => {
   const report_reader = (cb) => {
-    fs.readFile(path.join(__dirname, 'myfile.report.json'), 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, 'report.report.json'), 'utf8', (err, data) => {
       let lighthouse_metrics = {};
       JSON.parse(data).reportCategories.forEach((element) => {
         lighthouse_metrics[`Lighthouse ${element.name}`] = element.score;
@@ -61,3 +57,14 @@ badge.loadFont(path.join(__dirname, 'assets', 'fonts', 'Verdana.ttf'), (err) => 
   report_reader(metrics_to_svg)
 });
 
+
+
+// async function lighthouse_badge() {
+//   const { stdout } = await exec('ls');
+//
+//   console.log('stdout:', stdout);
+// }
+//
+// lighthouse_badge();
+
+// ./node_modules/.bin/lighthouse --output=json --quiet https://siroop.ch --chrome-flags='--headless' --output json --output-path ./report.json
