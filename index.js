de5592b665854425c966f87d63a6072466e96de0
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-10-15 13:40:57 +0200

    Add simple test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const COLOR_RANGES = {
   0: 'red',
 };
 
-const percentage_to_color = async (percentage) => {
+async function percentage_to_color(percentage) {
   let highest = 0;
   let color = COLOR_RANGES[0];
 
@@ -27,7 +27,7 @@ const percentage_to_color = async (percentage) => {
   }
 
   return color;
-};
+}
 
 
 async function metrics_to_svg(lighthouse_metrics) {
@@ -37,7 +37,7 @@ async function metrics_to_svg(lighthouse_metrics) {
 
     badge.loadFont(path.join(__dirname, 'assets', 'fonts', 'Verdana.ttf'), (err) => {
       badge({text: badge_text, colorscheme: badge_color, template: 'flat'}, (svg, err) => {
-        fs.writeFile(path.join(path.dirname(fs.realpathSync(__filename)), description.replace(/ /g,"_") + '.svg'), svg, (err) => {
+        fs.writeFile(path.join(path.dirname(fs.realpathSync(__filename)), description.replace(/ /g, "_") + '.svg'), svg, (err) => {
           if (err) {
             return console.log(err);
           }
@@ -48,15 +48,28 @@ async function metrics_to_svg(lighthouse_metrics) {
 }
 
 
-async function lighthouse_badge(url) {
+async function get_lighthouse_score(url) {
   const lighthouse_command = `./node_modules/.bin/lighthouse --quiet ${url} --chrome-flags='--headless'`;
   const {stdout} = await exec(lighthouse_command + ' --output=json --output-path=stdout', {maxBuffer: 1024 * 5000});
   let lighthouse_metrics = {};
   for (let element of  JSON.parse(stdout).reportCategories) {
     lighthouse_metrics[`Lighthouse ${element.name}`] = element.score;
   }
-  console.log('Finished parsing lighthouse data');
-  await metrics_to_svg(lighthouse_metrics)
+  return lighthouse_metrics
+}
+
+
+async function get_average_score(metrics) {
+  let lighthouse_metrics = {};
+  for (let metric of Object.keys(metrics[0])) {
+    let i = 0;
+    let total = 0;
+    for (; i < metrics.length; i++) {
+      total += metrics[i][metric];
+    }
+    lighthouse_metrics[metric] = total / i;
+  }
+  return lighthouse_metrics;
 }
 
 
@@ -64,6 +77,15 @@ async function lighthouse_badge(url) {
   if (process.argv.length < 3) {
     console.error('Please provide a url to perform lighthouse test')
   } else {
-    await lighthouse_badge(process.argv[2]);
+    let metrics = [];
+
+    for (let i = 2; i < process.argv.length; i++) {
+      metrics.push(await get_lighthouse_score(process.argv[i]));
+      console.log(`Metrics: ${metrics}`)
+    }
+
+    await metrics_to_svg(await get_average_score(metrics))
   }
 })();
+
+module.exports = {get_average_score};
