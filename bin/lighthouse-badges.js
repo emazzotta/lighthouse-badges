2251edf254cea2e4f7b560135570a8ea4f784095
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-10-16 18:53:55 +0200

    Introduce linter, fix most errors

--- bin/lighthouse-badges.js
+++ bin/lighthouse-badges.js
@@ -4,36 +4,45 @@ const badge = require('gh-badges');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const {percentage_to_color, get_average_score} = require('../lib/calculations');
+const { percentageToColor, getAverageScore } = require('../lib/calculations');
 const exec = util.promisify(require('child_process').exec);
 
 
-async function metrics_to_svg(lighthouse_metrics) {
-  for (let description of Object.keys(lighthouse_metrics)) {
-    const badge_color = await percentage_to_color(lighthouse_metrics[description]);
-    const badge_text = [description, Math.round(lighthouse_metrics[description]) + '%'];
+async function metricsToSvg(lighthouseMetrics) {
+  for (const description of Object.keys(lighthouseMetrics)) {
+    const badgeColor = percentageToColor(lighthouseMetrics[description]);
+    const badgeText = [description, `${Math.round(lighthouseMetrics[description])}%`];
 
-    badge.loadFont(path.join(__dirname, 'assets', 'fonts', 'Verdana.ttf'), (err) => {
-      badge({text: badge_text, colorscheme: badge_color, template: 'flat'}, (svg, err) => {
-        fs.writeFile(path.join(process.cwd(), description.replace(/ /g, "_") + '.svg'), svg, (err) => {
+    badge.loadFont(path.join(__dirname, '..', 'assets', 'fonts', 'Verdana.ttf'), (err) => {
+      if (err) {
+        throw err;
+      }
+      badge({ text: badgeText, colorscheme: badgeColor, template: 'flat' }, (svg, err) => {
+        if (err) {
+          throw err;
+        }
+        const filepath = path.join(process.cwd(), `${description.replace(/ /g, '_')}.svg`);
+        fs.writeFile(filepath, svg, (err) => {
           if (err) {
             return console.log(err);
           }
-        })
+          return console.log(`Saved file to ${filepath}`);
+        });
       });
     });
   }
 }
 
 
-async function get_lighthouse_score(url) {
-  const lighthouse_command = `${path.join(__dirname, '..', 'node_modules', '.bin', 'lighthouse')} --quiet ${url} --chrome-flags='--headless'`;
-  const {stdout} = await exec(lighthouse_command + ' --output=json --output-path=stdout', {maxBuffer: 1024 * 5000});
-  let lighthouse_metrics = {};
-  for (let element of  JSON.parse(stdout).reportCategories) {
-    lighthouse_metrics[`Lighthouse ${element.name}`] = element.score;
+async function getLighthouseScore(url) {
+  const lighthouseMetrics = {};
+  const lighthouseCommand = `${path.join(__dirname, '..', 'node_modules', '.bin', 'lighthouse')} --quiet ${url} --chrome-flags='--headless'`;
+  const { stdout } = await exec(`${lighthouseCommand} --output=json --output-path=stdout`, { maxBuffer: 1024 * 5000 });
+  const { reportCategories } = JSON.parse(stdout);
+  for (let i = 0; i < reportCategories.length; i += 1) {
+    lighthouseMetrics[`Lighthouse ${reportCategories[i].name}`] = reportCategories[i].score;
   }
-  return lighthouse_metrics
+  return lighthouseMetrics;
 }
 
 
@@ -42,10 +51,10 @@ async function get_lighthouse_score(url) {
     console.error('Please provide a url to perform lighthouse test');
   } else {
     console.log('Lighthouse performance test running... (this might take a while)');
-    let metrics = [];
-    for (let i = 2; i < process.argv.length; i++) {
-      metrics.push(await get_lighthouse_score(process.argv[i]));
+    const metrics = [];
+    for (let i = 2; i < process.argv.length; i += 1) {
+      metrics.push(await getLighthouseScore(process.argv[i]));
     }
-    await metrics_to_svg(await get_average_score(metrics))
+    await metricsToSvg(await getAverageScore(metrics));
   }
-})();
+}());
--- test/calculations.js
+++ test/calculations.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
-const {get_average_score} = require('../lib/calculations');
+const { getAverageScore } = require('../lib/calculations');
 
-describe('Calculations work', function () {
-  describe('the average is calculated correctly', function () {
-    it('should contain the expected average', async function () {
-      const expected_result = {'Lighthouse Acc': 60, 'Lighthouse Perf': 51};
+describe('Calculations work', () => {
+  describe('the average is calculated correctly', () => {
+    it('should contain the expected average', async () => {
+      const expectedResult = { 'Lighthouse Acc': 60, 'Lighthouse Perf': 51 };
       const input = [
-        {'Lighthouse Acc': 100, 'Lighthouse Perf': 52},
-        {'Lighthouse Acc': 20, 'Lighthouse Perf': 50}
+        { 'Lighthouse Acc': 100, 'Lighthouse Perf': 52 },
+        { 'Lighthouse Acc': 20, 'Lighthouse Perf': 50 },
       ];
-      const actual_result = await get_average_score(input);
-      assert.deepEqual(expected_result, actual_result)
+      const actualResult = await getAverageScore(input);
+      assert.deepEqual(expectedResult, actualResult);
     });
   });
 });
