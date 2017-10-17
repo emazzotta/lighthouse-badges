6d088221e7f5a705575f5e8ac00b863cac8b7aef
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-10-17 10:18:19 +0200

    Add test for squashed score

--- test/calculations.js
+++ test/calculations.js
@@ -1,15 +1,27 @@
 const assert = require('assert');
-const { getAverageScore } = require('../lib/calculations');
+const { getAverageScore, getSquashedScore } = require('../lib/calculations');
 
 describe('Calculations work', () => {
   describe('the average is calculated correctly', () => {
     it('should contain the expected average', async () => {
-      const expectedResult = { 'Lighthouse Acc': 60, 'Lighthouse Perf': 51 };
+      const expectedResult = { 'lighthouse accessibility': 60, 'lighthouse performance': 51 };
       const input = [
-        { 'Lighthouse Acc': 100, 'Lighthouse Perf': 52 },
-        { 'Lighthouse Acc': 20, 'Lighthouse Perf': 50 },
+        { 'lighthouse accessibility': 100, 'lighthouse performance': 52 },
+        { 'lighthouse accessibility': 20, 'lighthouse performance': 50 },
       ];
       const actualResult = await getAverageScore(input);
+      assert.deepEqual(expectedResult, actualResult);
+    });
+  });
+
+  describe('the average is calculated correctly for squashed score', () => {
+    it('should contain the expected squashed average', async () => {
+      const expectedResult = { lighthouse: 56 };
+      const input = [
+        { 'lighthouse accessibility': 100, 'lighthouse performance': 52 },
+        { 'lighthouse accessibility': 20, 'lighthouse performance': 50 },
+      ];
+      const actualResult = await getSquashedScore(input);
       assert.deepEqual(expectedResult, actualResult);
     });
   });
