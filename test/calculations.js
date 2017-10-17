55d139813c9a3c56fc8d2b8f944c3e05633d417a
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2017-10-17 14:59:27 +0200

    Add tests for rounding

--- test/calculations.js
+++ test/calculations.js
@@ -12,15 +12,49 @@ describe('Calculations work', () => {
       const actualResult = await getAverageScore(input);
       assert.deepEqual(expectedResult, actualResult);
     });
+
+    it('should round the expected average correctly', async () => {
+      const expectedResult = {
+        'lighthouse accessibility': 99.5,
+        'lighthouse performance': 99.5,
+        'lighthouse progressive web app': 99.5,
+        'lighthouse best practices': 99.5,
+      };
+      const input = [{
+        'lighthouse accessibility': 100,
+        'lighthouse performance': 100,
+        'lighthouse progressive web app': 100,
+        'lighthouse best practices': 100,
+      },{
+        'lighthouse accessibility': 99,
+        'lighthouse performance': 99,
+        'lighthouse progressive web app': 99,
+        'lighthouse best practices': 99,
+      },];
+      const actualResult = await getAverageScore(input);
+      assert.deepEqual(expectedResult, actualResult);
+    });
   });
 
   describe('the average is calculated correctly for squashed score', () => {
     it('should contain the expected squashed average', async () => {
-      const expectedResult = { lighthouse: 55.5 };
+      const expectedResult = { lighthouse: 60 };
       const input = [
-        { 'lighthouse accessibility': 100, 'lighthouse performance': 52 },
-        { 'lighthouse accessibility': 20, 'lighthouse performance': 50 },
+        { 'lighthouse accessibility': 100, 'lighthouse performance': 65 },
+        { 'lighthouse accessibility': 20, 'lighthouse performance': 55 },
       ];
+      const actualResult = await getSquashedScore(input);
+      assert.deepEqual(expectedResult, actualResult);
+    });
+
+    it('should round the expected squashed average correctly', async () => {
+      const expectedResult = { lighthouse: 82.5 };
+      const input = [{
+        'lighthouse accessibility': 100,
+        'lighthouse performance': 100,
+        'lighthouse progressive web app': 55,
+        'lighthouse best practices': 75
+      },];
       const actualResult = await getSquashedScore(input);
       assert.deepEqual(expectedResult, actualResult);
     });
