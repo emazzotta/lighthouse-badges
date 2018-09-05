6a73230aa8cd17409bc0189787a9cbbbf4983c37
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2018-09-05 17:10:09 +0200

    Use more es6 like syntax

--- bin/main.js
+++ bin/main.js
@@ -6,12 +6,10 @@ const { parser } = require('../lib/argparser');
 
 (async function () {
   const args = await parser.parseArgs();
-  const promisesToAwait = [];
-  for (let i = 0; i < args.urls.length; i += 1) {
-    promisesToAwait.push(getLighthouseMetrics(args.urls[i], args.save_report));
-  }
   console.log('Lighthouse performance test running... (this might take a while)');
-  const results = await Promise.all(promisesToAwait);
+  const results = await Promise.all(
+    args.urls.map(url => getLighthouseMetrics(url, args.save_report)),
+  );
   const metrics = results.map(result => result.metrics);
   const reports = results.map(result => result.report);
   const metricsResults = args.single_badge === true
