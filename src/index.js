f1701c29b3d3de160d1dd8bd9f8ceba42f281862
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2024-06-15 06:14:48 +0200

    Improve handle user input

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import handleUserInput from "./main.js";
 import CLI from "clui";
 
 const CLI_SPINNER = new CLI.Spinner('Running Lighthouse, please wait...', ['◜', '◠', '◝', '◞', '◡', '◟']);
-handleUserInput(CLI_SPINNER).then(() => process.exit(0));
+handleUserInput(CLI_SPINNER).then(() => {});
