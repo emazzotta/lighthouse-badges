90167349a91b2f5a174b276394313f62496fd614
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2025-11-08 22:41:17 +0100

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
 import eslint from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default [
   {
     ignores: ["coverage/", "dist/", ".idea/", "node_modules/"]
   },
   eslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
   {
     files: ["**/*.ts"],
     languageOptions: {
@@ -15,20 +24,33 @@ export default [
         ecmaVersion: 2022,
         sourceType: "module",
       },
+      globals: {
+        ...globals.node,
+      },
     },
     plugins: {
       "@typescript-eslint": tseslint,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      "no-underscore-dangle": "warn",
+      "no-underscore-dangle": ["warn", { "allow": ["__filename", "__dirname"] }],
       "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "no-undef": "off",
+    },
+  },
+  {
+    files: ["**/__tests__/**/*.ts", "**/*.test.ts", "**/*.spec.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
   },
   {
     files: ["**/*.js"],
     rules: {
-      "no-underscore-dangle": "warn",
+      "no-underscore-dangle": ["warn", { "allow": ["__filename", "__dirname"] }],
       "no-undef": "off",
     },
   }
